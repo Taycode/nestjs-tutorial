92afc9de2b3543e5e6df127fc1677830a5dba0b9
Abdulmateen Tairu <tay2druh@gmail.com>
2021-10-15 17:07:38 +0100

    Authoriation Guard

--- src/authorization/authorization.guard.ts
+++ src/authorization/authorization.guard.ts
@@ -1,11 +1,38 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { expressJwtSecret } from 'jwks-rsa';
+import { promisify } from 'util';
+import * as jwt from 'express-jwt';
 
 @Injectable()
 export class AuthorizationGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.getArgByIndex(0);
+    const res = context.getArgByIndex(1);
+
+    const checkJwt = promisify(
+      jwt({
+        secret: expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: '',
+        }),
+        audience: null,
+        issuer: null,
+        algorithms: ['RSA256'],
+      }),
+    );
+    try {
+      await checkJwt(req, res);
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
     return true;
   }
 }
